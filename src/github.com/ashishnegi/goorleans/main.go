c8c7ec8d2c4f3d78504469c43dc9972e4c4140d5
Ashish Negi <ashish@quintype.com>
2016-11-01 16:40:03 +0530

    learning some syntax: function returning two variables.

--- src/github.com/ashishnegi/goorleans/main.go
+++ src/github.com/ashishnegi/goorleans/main.go
@@ -5,6 +5,17 @@ import (
 	"github.com/ashishnegi/stringutils"
 )
 
+func returnTwoValues(num int) (int, int) {
+	return num / 2, num / 3
+}
+
 func main() {
-	fmt.Printf(stringutils.Reverse("Hello, go !!!.\n"))
+	var clojure, haskell = "clojure: yo!!\n", "haskell: hell yaa!!!\n"
+	fst, _ := returnTwoValues(100)
+
+	fmt.Println(stringutils.Reverse("Hello, go !!!."),
+		"\n",
+		haskell,
+		clojure,
+		fst)
 }
