57549dec7c567c7d64321ba0466c32e512dbd593
Ashish Negi <ashishnegi33@gmail.com>
2018-03-20 07:35:03 +0530

    Search across replicas

--- search-engine/search.go
+++ search-engine/search.go
@@ -10,9 +10,12 @@ import (
 
 // define file scoped variables.
 var (
-	WebSearch   = fakeSearch("web-server")
-	ImageSearch = fakeSearch("image-server")
-	VideoSearch = fakeSearch("video-server")
+	WebSearch1   = fakeSearch("web-server1")
+	WebSearch2   = fakeSearch("web-server2")
+	ImageSearch1 = fakeSearch("image-server1")
+	ImageSearch2 = fakeSearch("image-server2")
+	VideoSearch1 = fakeSearch("video-server1")
+	VideoSearch2 = fakeSearch("video-server2")
 )
 
 // typedef alias
@@ -32,7 +35,18 @@ func fakeSearch(server string) Search {
 	}
 }
 
-// Bing2.0
+func firstResult(query string, replicas ...Search) Result {
+	resultChannel := make(chan Result)
+	for i := range replicas {
+		// gotcha // passing the i as parameter..
+		go func(nested_i int) { resultChannel <- replicas[nested_i](query) }(i)
+	}
+	return <-resultChannel
+}
+
+// Bing3.0
+// search results within a time frame across replicated clusters.
+// with no locks, no condition variables, no callbacks.
 func Bing(query string) (results []Result) {
 	// channel is first class citizen. you can pass them around like values.
 	// make a type safe channel.
@@ -40,9 +54,9 @@ func Bing(query string) (results []Result) {
 
 	// go func() {... } () ==> starts function on a go routine.
 	// to put value on a channel ==> channelName <- value.
-	go func() { resultsChannel <- WebSearch(query) }()
-	go func() { resultsChannel <- ImageSearch(query) }()
-	go func() { resultsChannel <- VideoSearch(query) }()
+	go func() { resultsChannel <- firstResult(query, WebSearch1, WebSearch2) }()
+	go func() { resultsChannel <- firstResult(query, ImageSearch1, ImageSearch2) }()
+	go func() { resultsChannel <- firstResult(query, VideoSearch1, VideoSearch2) }()
 
 	// make a timeout of 70 milliseconds for complete query.
 	timeout := time.After(70 * time.Millisecond)
@@ -55,7 +69,7 @@ func Bing(query string) (results []Result) {
 			fmt.Println("Request timedout")
 			return
 			// all channels will be garbage collected.
-			// but not go routines.
+			// but not go routines. // they will be lingering around untill web search is complete.
 		}
 	}
 	// channel is garbage collected.
